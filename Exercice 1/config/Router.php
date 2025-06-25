4983baf940a354cb73360bc6a1a31758a79adad6
Husseina <husseinaaoudoupacco@3wa.io>
2025-06-25 13:51:36 +0000

    2

--- Exercice 1/config/Router.php
+++ Exercice 1/config/Router.php
@@ -1,27 +1,29 @@
 <?php
  class Router{
-     public function handleRequest($route){
+     public function handleRequest($route) {
+         $controller = new UserController();
+         
          switch ($route){
              case 'show_user':
-                 // UserController:: show()
+                 $controller-> show();
                  break;
              case 'create_user':
-                 // UserController:: create()
+                 $controller-> create();
                  break;
              case 'check_create_user':
-                 // UserController:: checkCreate()
+                 $controller->checkCreate();
                  break;
             case 'update_user':
-                 // UserController:: update()
+                 $controller->update();
                  break;
             case 'check_update_user':
-                 // UserController:: checkUpdate()
+                 $controller->checkUpdate();
                  break;
             case 'delete_user':
-                 // UserController:: delete()
+                 $controller-> delete();
                  break;
             default:
-                 // UserController::list()
+                 $controller->list();
                  break;
          }
          
