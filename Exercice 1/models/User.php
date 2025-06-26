c98ba2e2d535db339a94a7f425886e1aef3262dd
Husseina <husseinaaoudoupacco@3wa.io>
2025-06-26 07:59:39 +0000

    CC

--- Exercice 1/models/User.php
+++ Exercice 1/models/User.php
@@ -1,13 +1,19 @@
 <?php
 class User{
-    private int $id;
+    private ?int $id = null;
     private string $email;
     private string $first_name;
     private string $last_name;
-    public function __construct(int $id, string $email, string $first_name, string $last_name){}
+    public function __construct(?int $id, string $email, string $first_name, string $last_name)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
     
-    public function getId(): int {return $this->id;}
-    public function setId($id): void {$this->id = $id;}
+    public function getId(): ?int {return $this->id;}
+    public function setId(?int $id): void {$this->id = $id;}
     public function getEmail(): string {return $this->email;}
     public function setEmail($email): void {$this->email = $email;}
     public function getFirstName(): string {return $this->first_name; }
