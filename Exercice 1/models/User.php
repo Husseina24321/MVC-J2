1130bde4e51b8c91f8e39407acbb01cf8a42129d
Husseina <husseinaaoudoupacco@3wa.io>
2025-06-26 07:46:10 +0000

    2J

--- Exercice 1/models/User.php
+++ Exercice 1/models/User.php
@@ -1,33 +1,17 @@
 <?php
 class User{
-    private $id;
-    private $email;
-    private $first_name;
-    private $last_name;
-    public function __construct(){}
+    private int $id;
+    private string $email;
+    private string $first_name;
+    private string $last_name;
+    public function __construct(int $id, string $email, string $first_name, string $last_name){}
     
-    public function getId() {
-        return $this->id;
-    }
-    public function setId($id) {
-        $this->id = $id;
-    }
-    public function getEmail() {
-        return $this->email;
-    }
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-    public function getFirstName() {
-        return $this->first_name;
-    }
-    public function setFirstName($first_name) {
-        $this->first_name = $first_name;
-    }
-    public function getLastName() {
-        return $this->last_name;
-    }
-    public function setLastName($last_name) {
-        $this->last_name = $last_name;
-    }
+    public function getId(): int {return $this->id;}
+    public function setId($id): void {$this->id = $id;}
+    public function getEmail(): string {return $this->email;}
+    public function setEmail($email): void {$this->email = $email;}
+    public function getFirstName(): string {return $this->first_name; }
+    public function setFirstName($first_name): void{ $this->first_name = $first_name;}
+    public function getLastName(): string {return $this->last_name;}
+    public function setLastName($last_name): void{$this->last_name = $last_name; }
 }
