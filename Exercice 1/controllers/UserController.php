9aef11e419c3f356f57e437bc880e42dddcda909
Husseina <husseinaaoudoupacco@3wa.io>
2025-06-26 14:10:44 +0000

    2g

--- Exercice 1/controllers/UserController.php
+++ Exercice 1/controllers/UserController.php
@@ -8,8 +8,32 @@ class UserController{
         $route = "templates/users/show.phtml";
         require "templates/layout.phtml";
     }
-     public function checkCreate(){
-        echo "creation de l'utilisateur";
+     public function checkCreate(): void{
+        if (
+        isset($_POST['email']) &&
+        isset($_POST['first_name']) &&
+        isset($_POST['last_name'])
+    ) {
+
+        $email = htmlspecialchars($_POST['email']);
+        $first_name = htmlspecialchars($_POST['first_name']);
+        $last_name = htmlspecialchars($_POST['last_name']);
+
+
+        $user = new User(null, $email, $first_name, $last_name);
+
+
+        $manager = new UserManager();
+        $manager->insert($user);
+
+
+        header('Location: index.php');
+        exit();
+    } else {
+
+        echo 'Tous les champs sont obligatoires.';
+        exit();
+    }
     }
     public function update(){
         $route = "templates/users/update.phtml";
