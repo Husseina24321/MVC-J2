3735e09c5b3b0ef34db1588ed2462ceb540df673
Husseina <husseinaaoudoupacco@3wa.io>
2025-06-26 09:22:11 +0000

    2b

--- Exercice 1/managers/AbstractManager.php
+++ Exercice 1/managers/AbstractManager.php
@@ -3,11 +3,17 @@
      protected PDO $db;
       public function __construct()
       {
+        $host = 'db.3wa.io'; 
+        $port = 3306; 
+        $dbname = 'husseinaaoudoupacco_crud_mvc';
+        $charset = 'utf8';
+        $user = 'husseinaaoudoupacco';
+        $password = "8acf2f659e6a8316844017f35f683ca8"; 
           try {
             $this->db = new PDO(
                 "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
-                "husseinaaoudoupacco_crud_mvc",
-                "8acf2f659e6a8316844017f35f683ca8"
+                $user,
+                $password
             );
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
